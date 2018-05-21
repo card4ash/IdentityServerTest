9a3fe5cb27bf944543d94f90482f7e490ae815c9
Ashraful Alam <ashraful.islam@aplombtechbd.com>
2018-05-22 01:27:05 +0600

    Identity server client web app

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+    public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+    private static async Task MainAsync()
+    {
+      // discover endpoints from metadata
+      Console.WriteLine("api check");
+      Console.ReadLine();
+      var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+      if (disco.IsError)
+      {
+        Console.WriteLine(disco.Error);
+        return;
+      }
+
+      //// request token
+      //var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+      //var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+
+      // request token
+      var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+      var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");
+
+      if (tokenResponse.IsError)
+      {
+        Console.WriteLine(tokenResponse.Error);
+        return;
+      }
+
+      if (tokenResponse.IsError)
+      {
+        Console.WriteLine(tokenResponse.Error);
+        return;
+      }
+
+      Console.WriteLine(tokenResponse.Json);
+      Console.WriteLine("\n\n");
+
+      // call api
+      var client = new HttpClient();
+      client.SetBearerToken(tokenResponse.AccessToken);
+
+      var response = await client.GetAsync("http://localhost:5001/identity");
+      if (!response.IsSuccessStatusCode)
+      {
+        Console.WriteLine(response.StatusCode);
+      }
+      else
+      {
+        var content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(JArray.Parse(content));
+      }
+      Console.WriteLine("done check");
+      Console.ReadLine();
+    }
+  }
+}
--- IdentityServerQuickTest/Startup.cs
+++ IdentityServerQuickTest/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityServerQuickTest
 {
@@ -16,7 +18,42 @@ namespace IdentityServerQuickTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+      // configure identity server with in-memory stores, keys, clients and resources
+          services.AddIdentityServer()
+          .AddDeveloperSigningCredential()
+          .AddInMemoryApiResources(Config.GetApiResources())
+          .AddInMemoryClients(Config.GetClients())
+          .AddTestUsers(Config.GetUsers()); 
+
+      //services.AddAuthentication()
+      //      .AddGoogle("Google", options =>
+      //      {
+      //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+      //        options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+      //        options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+      //      })
+      //      .AddOpenIdConnect("demoidsrv", "IdentityServer", options =>
+      //      {
+      //        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+      //        options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+      //        options.Authority = "https://demo.identityserver.io/";
+      //        options.ClientId = "implicit";
+      //        options.ResponseType = "id_token";
+      //        options.SaveTokens = true;
+      //        options.CallbackPath = new PathString("/signin-idsrv");
+      //        options.SignedOutCallbackPath = new PathString("/signout-callback-idsrv");
+      //        options.RemoteSignOutPath = new PathString("/signout-idsrv");
+
+      //        options.TokenValidationParameters = new TokenValidationParameters
+      //        {
+      //          NameClaimType = "name",
+      //          RoleClaimType = "role"
+      //        };
+      //      });
+      
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -29,6 +66,7 @@ namespace IdentityServerQuickTest
             app.UseStaticFiles();
 
         app.UseIdentityServer();
+
 
         app.UseMvcWithDefaultRoute();
         }
